d020dfed2a51a561d7bba2a7a1c0be0f092f8190
Olly Headey <olly@headey.org>
2025-05-08 20:30:44 +0100

    Another build process change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,njk,md,js}",
     "./content/**/*.{html,njk,md}",
     "./index.md"
   ],
-  theme: {
-    extend: {
-      colors: {
-        'eps-blue': '#1e40af',
-        'eps-gray': '#374151',
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-        serif: ['Georgia', 'serif'],
-      },
-      fontSize: {
-        '3xl': '1.875rem',
-        '4xl': '2.25rem'
-      }
-    },
-  },
   plugins: [
     require('@tailwindcss/typography')
   ]
